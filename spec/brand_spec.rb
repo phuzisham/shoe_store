16499f1a0b165cdc12905b62c4f9c4549ab80e42
David Hale <david.hale7@gmail.com>
2017-09-29 12:52:29 -0700

    callback for capitalize on brand and store

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,13 +2,20 @@ require('spec_helper')
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores) }
+
   it("ensures the length of title is at most 40 characters") do
     brand = Brand.new({:title => "a".*(41)})
     expect(brand.save()).to(eq(false))
   end
+
   it("ensures that title doesn't already exist") do
     Brand.create({:title => 'Croc Larkins'})
     brand = Brand.new({:title => 'croc larkins'})
     expect(brand.save()).to(eq(false))
+  end
+
+  it('will capitalize title') do
+    brand = Brand.create({:title => 'croc larkins'})
+    expect(brand.title).to(eq('Croc Larkins'))
   end
 end
