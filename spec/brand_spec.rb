2cfecfdd11034cf863c8a68552ff8aa9b301c24b
David Hale <david.hale7@gmail.com>
2017-09-29 09:20:17 -0700

    validation specs passing

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,13 +2,13 @@ require('spec_helper')
 
 describe(Brand) do
   it { should have_and_belong_to_many(:stores) }
-  # it("ensures the length of title is at most 40 characters") do
-  #   brand = Brand.new({:title => "a".*(41)})
-  #   expect(brand.save()).to(eq(false))
-  # end
-  # it("ensures that title doesn't already exist") do
-  #   Brand.create({:title => 'Croc Larkins'})
-  #   brand = Brand.new({:title => 'Croc Larkins'})
-  #   expect(brand.save()).to(eq(false))
-  # end
+  it("ensures the length of title is at most 40 characters") do
+    brand = Brand.new({:title => "a".*(41)})
+    expect(brand.save()).to(eq(false))
+  end
+  it("ensures that title doesn't already exist") do
+    Brand.create({:title => 'Croc Larkins'})
+    brand = Brand.new({:title => 'Croc Larkins'})
+    expect(brand.save()).to(eq(false))
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,13 +2,13 @@ require('spec_helper')
 
 describe(Store) do
   it { should have_and_belong_to_many(:brands) }
-  # it("ensures the length of title is at most 40 characters") do
-  #   store = Store.new({:title => "a".*(41)})
-  #   expect(store.save()).to(eq(false))
-  # end
-  # it("ensures that title doesn't already exist") do
-  #   store.create({:title => 'Jim\'s Shoes'})
-  #   store = Store.new({:title => 'Jim\'s Shoes'})
-  #   expect(store.save()).to(eq(false))
-  # end
+  it("ensures the length of title is at most 40 characters") do
+    store = Store.new({:title => "a".*(41)})
+    expect(store.save()).to(eq(false))
+  end
+  it("ensures that title doesn't already exist") do
+    Store.create({:title => 'Jim\'s Shoes'})
+    store = Store.new({:title => 'Jim\'s Shoes'})
+    expect(store.save()).to(eq(false))
+  end
 end
